55ff9114ec35e2fd870870b7547c63fc7e961d47
alejandropiccardo <110832144+alejandropiccardo@users.noreply.github.com>
2022-08-29 10:42:26 -0300

    Update Libro.cs

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -18,16 +18,21 @@ namespace SRP
             this.Code = code;
         }
 
-        public void ShelveBook(String sector, String shelve)
-            /*deberia ser una clase aparte por si cambia el metodo de almacenamiento de libros, sea más facil de modificar,
+        /*public void ShelveBook(String sector, String shelve)
+            deberia ser una clase aparte por si cambia el metodo de almacenamiento de libros, sea más facil de modificar,
             ej: la biblioteca se expande y ya no solo se necesita 1 sector y 1 estante, sino que hay libros almacenados en central y otros en mulling,
             y se necesita identificar con una "c" o una "m"
-        */
+        
         {
-            //this.LibraryBuilding ...
+            
             this.LibrarySector = sector;
             this.LibraryShelve = shelve;
-        }
-
+        }*/
     }
+    public class ShelveBook(Book Book2Shelve)
+    {
+    
+    }
+    
+    
 }
